a488b47ab6d62b0df2a0643f9a7313adc15fe9b7
neal <nealhallyoung.buffer@outlook.com>
2024-12-29 20:42:38 +0800

    [feature](vimreg): add __call_tapi_reg()
    print format string

--- macros/vimreg.sh
+++ macros/vimreg.sh
@@ -9,3 +9,19 @@ vimreg() {
     return 2
   fi
 }
+
+__call_tapi_reg() {
+  local cmd
+  local arg1
+  local arg2
+  # -n 输出参数的值，不添加换行符
+  # -R 表示原始输入模式，直接处理原始输入
+  # --slurp 将所有输入合并为一个数组
+  # . 表示输入的原始内容
+  cmd=$(echo  -n "$1" | jq -R --slurp .)
+  arg1=$(echo -n "$2" | jq -R --slurp .)
+  arg2=$(echo -n "$3" | jq -R --slurp .)
+  # \e]51; 这是一个控制字符序列，表示终端的自定义控制序列
+  # \x07 这是 ASCII 控制字符 BEL（响铃），用于标识控制序列的结束
+  printf '\e]51;["call","Tapi_reg",[%s,%s,%s]]\x07' "${cmd}" "${arg1}" "${arg2}"
+}
