2809451871a8d148eec8f17e56a9e8e65fab3a62
nealhallyoung <nealhallyoung.buffer@outlook.com>
2025-01-18 18:52:43 +0800

    [feature](vimreg): add clip() to simulate clipboard cmd such as xclip

--- macros/vimreg.sh
+++ macros/vimreg.sh
@@ -1,3 +1,26 @@
+clip() {
+  if [[ -z "$CLIP" ]]; then
+    echo "error: CLIP environment variable is not defined. Please set it to a file path."
+    return 1
+  fi
+  if [[ -t 0 ]]; then
+    # 如果没有从标准输入读取数据，则是输出剪贴板内容到文件
+    if [[ -f "$CLIP" ]]; then
+      # 如果文件存在但为空
+      if [[ ! -s "$CLIP" ]]; then
+        echo "Clipboard is empty"
+      else
+        cat "$CLIP"
+      fi
+    else
+      echo "error: .clip file not found"
+    fi
+  else
+    # 如果从标准输入读取数据，则将数据写入剪贴板文件
+    cat >"$CLIP"
+  fi
+}
+
 vimreg() {
   if ! command -v jq >/dev/null 2>&1; then
     echo 'error: jq is not installed' >&2
@@ -29,7 +52,6 @@ vimreg() {
   *) ;;
   esac
   [ "${reg}" != '--list' ] && [ $# -gt 0 ] && reg=$1
-
   local file
   file=$(mktemp -u)
   # shellcheck disable=SC2064
@@ -61,5 +83,6 @@ __call_tapi_reg() {
   arg2=$(echo -n "$3" | jq -R --slurp .)
   # \e]51; 这是一个控制字符序列，表示终端的自定义控制序列
   # \x07 这是 ASCII 控制字符 BEL（响铃），用于标识控制序列的结束
+  # 通过向终端发送一个特定的控制序列来调用 Vim 内部的函数。
   printf '\e]51;["call","Tapi_reg",[%s,%s,%s]]\x07' "${cmd}" "${arg1}" "${arg2}"
 }
